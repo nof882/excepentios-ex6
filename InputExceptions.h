e3d024b1bdb11167676a0442b6de701b245b8a55
Nachshon Fisher <babyoda881@gmail.com>
2025-12-10 07:47:26 +0200

    fixing InputExceptions

--- InputExceptions.h
+++ InputExceptions.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <exception>
+#include <string>
 
 class InputException : public std::exception {
+    std::string msg;
 public:
+    InputException(const std::string& m) : msg(m) {}
     const char* what() const noexcept override {
-        return "Invalid input: expected a number.";
+        return msg.c_str();
     }
 };
