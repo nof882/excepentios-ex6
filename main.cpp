e9f2e1c0c357da154382122222b08bc777027805
Nachshon Fisher <babyoda881@gmail.com>
2025-12-10 07:43:10 +0200

    updated the main

--- main.cpp
+++ main.cpp
@@ -4,90 +4,111 @@
 #include "Rectangle.h"
 #include "Parallelogram.h"
 #include "ShapeException.h"
+#include "InputException.h"
 #include <string>
 #include <iostream>
 
+void checkNumericInput() {
+    if (std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        throw InputException("Invalid numeric input");
+    }
+}
+
 int main()
 {
-	std::string nam, col; double rad = 0, ang = 0, ang2 = 180; int height = 0, width = 0;
-	Circle circ(col, nam, rad);
-	Quadrilateral quad(nam, col, width, height);
-	rectangle rec(nam, col, width, height);
-	Parallelogram para(nam, col, width, height, ang, ang2);
+    std::string nam, col;
+    double rad = 0, ang = 0, ang2 = 180;
+    int height = 0, width = 0;
 
-	Shape *ptrcirc = &circ;
-	Shape *ptrquad = &quad;
-	Shape *ptrrec = &rec;
-	Shape *ptrpara = &para;
+    Circle circ(col, nam, rad);
+    Quadrilateral quad(nam, col, width, height);
+    rectangle rec(nam, col, width, height);
+    Parallelogram para(nam, col, width, height, ang, ang2);
 
+    Shape* ptrcirc = &circ;
+    Shape* ptrquad = &quad;
+    Shape* ptrrec = &rec;
+    Shape* ptrpara = &para;
 
-	
-	std::cout << "Enter information for your objects" << std::endl;
-	const char circle = 'c', quadrilateral = 'q', rectangle = 'r', parallelogram = 'p'; char shapetype;
-	char x = 'y';
-	while (x != 'x') {
-		std::cout << "which shape would you like to work with?.. \nc=circle, q = quadrilateral, r = rectangle, p = parallelogram" << std::endl;
-		std::cin >> shapetype;
-try
-		{
+    std::cout << "Enter information for your objects\n";
+    char shapetype;
+    char x = 'y';
 
-			switch (shapetype) {
-			case 'c':
-				std::cout << "enter color, name,  rad for circle" << std::endl;
-				std::cin >> col >> nam >> rad;
-				circ.setColor(col);
-				circ.setName(nam);
-				circ.setRad(rad);
-				ptrcirc->draw();
-				break;
-			case 'q':
-				std::cout << "enter name, color, height, width" << std::endl;
-				std::cin >> nam >> col >> height >> width;
-				quad.setName(nam);
-				quad.setColor(col);
-				quad.setHeight(height);
-				quad.setWidth(width);
-				ptrquad->draw();
-				break;
-			case 'r':
-				std::cout << "enter name, color, height, width" << std::endl;
-				std::cin >> nam >> col >> height >> width;
-				rec.setName(nam);
-				rec.setColor(col);
-				rec.setHeight(height);
-				rec.setWidth(width);
-				ptrrec->draw();
-				break;
-			case 'p':
-				std::cout << "enter name, color, height, width, 2 angles" << std::endl;
-				std::cin >> nam >> col >> height >> width >> ang >> ang2;
-				para.setName(nam);
-				para.setColor(col);
-				para.setHeight(height);
-				para.setWidth(width);
-				para.setAngle(ang, ang2);
-				ptrpara->draw();
+    while (x != 'x') {
+        std::cout << "c=circle, q=quadrilateral, r=rectangle, p=parallelogram\n";
+        std::cin >> shapetype;
 
-			default:
-				std::cout << "you have entered an invalid letter, please re-enter" << std::endl;
-				break;
-			}
-			std::cout << "would you like to add more object press any key if not press x" << std::endl;
-			std::cin.get() >> x;
-		}
-		catch (std::exception e)
-		{			
-			printf(e.what());
-		}
-		catch (...)
-		{
-			printf("caught a bad exception. continuing as usual\n");
-		}
-	}
+        try {
+            switch (shapetype) {
 
+            case 'c':
+                std::cout << "enter color, name, rad\n";
+                std::cin >> col >> nam >> rad;
+                checkNumericInput();
+                circ.setColor(col);
+                circ.setName(nam);
+                circ.setRad(rad);
+                ptrcirc->draw();
+                break;
 
+            case 'q':
+                std::cout << "enter name, color, height, width\n";
+                std::cin >> nam >> col >> height >> width;
+                checkNumericInput();
+                quad.setName(nam);
+                quad.setColor(col);
+                quad.setHeight(height);
+                quad.setWidth(width);
+                ptrquad->draw();
+                break;
 
-		system("pause");
-		return 0;
-	
+            case 'r':
+                std::cout << "enter name, color, height, width\n";
+                std::cin >> nam >> col >> height >> width;
+                checkNumericInput();
+                rec.setName(nam);
+                rec.setColor(col);
+                rec.setHeight(height);
+                rec.setWidth(width);
+                ptrrec->draw();
+                break;
+
+            case 'p':
+                std::cout << "enter name, color, height, width, angle1, angle2\n";
+                std::cin >> nam >> col >> height >> width >> ang >> ang2;
+                checkNumericInput();
+                para.setName(nam);
+                para.setColor(col);
+                para.setHeight(height);
+                para.setWidth(width);
+                para.setAngle(ang, ang2);
+                ptrpara->draw();
+                break;
+
+            default:
+                std::cout << "invalid shape type\n";
+                break;
+            }
+
+            std::cout << "add more? press any key, x to exit\n";
+            std::cin >> x;
+        }
+
+        catch (InputException& e) {
+            std::cout << "INPUT ERROR: " << e.what() << "\n";
+            continue;
+        }
+
+        catch (ShapeException& e) {
+            std::cout << "SHAPE ERROR: " << e.what() << "\n";
+        }
+
+        catch (std::exception& e) {
+            std::cout << "GENERAL ERROR: " << e.what() << "\n";
+        }
+    }
+
+    return 0;
 }
